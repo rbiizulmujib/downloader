d220bfc64b099c9301e0faa6d5f9f657a52677d7
rbiizulmujib <37804178+rbiizulmujib@users.noreply.github.com>
2022-04-30 14:18:02 +0700

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,63 @@
+# import the necessary packages
+from imutils import paths
+import argparse
+import requests
+import cv2
+import os
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+}
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-u", "--urls", required=True,
+	help="path to file containing image URLs")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output directory of images")
+args = vars(ap.parse_args())
+# grab the list of URLs from the input file, then initialize the
+# total number of images downloaded thus far
+rows = open(args["urls"]).read().strip().split("\n")
+total = 0
+
+# loop the URLs
+for url in rows:
+	try:
+		# try to download the image
+		r = requests.get(url, headers=headers, timeout=60)
+		# save the image to disk
+		p = os.path.sep.join([args["output"], "{}.jpg".format(
+			str(total).zfill(8))])
+		f = open(p, "wb")
+		f.write(r.content)
+		f.close()
+		# update the counter
+		print("[INFO] downloaded: {}".format(p))
+		total += 1
+	# handle if any exceptions are thrown during the download process
+	except:
+		print("[INFO] error downloading {}...skipping".format(p))
+
+# loop over the image paths we just downloaded
+for imagePath in paths.list_images(args["output"]):
+	# initialize if the image should be deleted or not
+	delete = False
+	# try to load the image
+	try:
+		image = cv2.imread(imagePath)
+		# if the image is `None` then we could not properly load it
+		# from disk, so delete it
+		if image is None:
+			delete = True
+	# if OpenCV cannot load the image then the image is likely
+	# corrupt so we should delete it
+	except:
+		print("Except")
+		delete = True
+	# check to see if the image should be deleted
+	if delete:
+		print("[INFO] deleting {}".format(imagePath))
+		os.remove(imagePath)
+
